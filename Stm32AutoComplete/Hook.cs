5cea15bc3c47be9dbdae3117d769f1d35df3a7aa
taoduhui <taoduhui@hotmail.com>
2022-03-24 15:29:34 +0800

    init

---
+++ Stm32AutoComplete/Hook.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stm32AutoComplete
+{
+    internal class Hook
+    {
+    }
+    public class KeyPressSender
+    {
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class KeyBoardHookStruct
+        {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+        //事件输出
+        public event KeyEventHandler KeyUpEvent;
+
+        //委托 
+        public delegate int HookProc(int nCode, int wParam, IntPtr lParam);
+        static int hHook = 0;
+        public const int WH_KEYBOARD_LL = 13;
+        //LowLevel键盘截获，如果是WH_KEYBOARD＝2，并不能对系统键盘截取，Acrobat Reader会在你截取之前获得键盘。 
+        static HookProc KeyBoardHookProcedure;
+
+        //设置钩子 
+        [DllImport("user32.dll")]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        //抽掉钩子 
+        public static extern bool UnhookWindowsHookEx(int idHook);
+        [DllImport("user32.dll")]
+        //调用下一个钩子 
+        public static extern int CallNextHookEx(int idHook, int nCode, int wParam, IntPtr lParam);
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string name);
+
+        public void Hook_Start()
+        {
+            if (hHook == 0)
+            {
+                KeyBoardHookProcedure = new HookProc(KeyBoardHookProc);
+                hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyBoardHookProcedure,
+                        GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName), 0);
+                //如果设置钩子失败. 
+                if (hHook == 0)
+                {
+                    Hook_Clear();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 取消钩子事件
+        /// </summary>
+        public void Hook_Clear()
+        {
+            bool retKeyboard = true;
+            if (hHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hHook);
+                hHook = 0;
+            }
+        }
+
+        public int KeyBoardHookProc(int nCode, int wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                KeyBoardHookStruct kbh = (KeyBoardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyBoardHookStruct));
+                Keys KeyData = (Keys)Enum.Parse(typeof(Keys), kbh.vkCode.ToString());
+                // 键盘抬起
+                if (KeyUpEvent != null)/* && wParam == WM_KEYUP)*/
+                {
+                    //Keys keyData = (Keys)MyKeyboardHookStruct.vkCode;
+                    //if (keyData == Keys.Up)
+                    //{
+                    KeyEventArgs e = new KeyEventArgs(KeyData);//绑定事件
+                    KeyUpEvent(this, e);
+                    //MessageBox.Show("捕捉到了按键释放");
+                    //}
+                }
+                #region 数据操作
+                //switch (k)
+                //{
+                //    case Keys.F2:
+                //        if (kbh.flags == 0)
+                //        {
+                //             
+
+                //        }
+                //        else if (kbh.flags == 128)
+                //        {
+                //            //放开后做什么事
+                //        }
+                //        return 1;
+                //}
+                #endregion
+            }
+            return CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
+
+    }
+
+}
---
+++ Stm32AutoComplete/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Stm32AutoComplete
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Forms.KeyEventHandler myKeyEventHandeler = null;//按键钩子
+                                                          //private KeyboardHook k_hook = new KeyboardHook();
+        private KeyPressSender k_hook = new KeyPressSender();
+
+        List<Keys> ActiveKey = new List<Keys>() { Keys.Q, Keys.W, Keys.E, Keys.R, Keys.T, Keys.Y, Keys.U, Keys.I, Keys.O, Keys.P, Keys.A, Keys.S, Keys.D, Keys.F, Keys.G, Keys.H, Keys.J, Keys.K, Keys.L, Keys.Z, Keys.X, Keys.C, Keys.V, Keys.B, Keys.N, Keys.M,Keys.OemMinus };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void hook_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (ActiveKey.Contains(e.KeyCode))
+            {
+                MoniAction.Keyboard.Press(Key.LeftAlt);
+                MoniAction.Keyboard.Press(Key.OemQuestion);
+                MoniAction.Keyboard.Release(Key.OemQuestion);
+                MoniAction.Keyboard.Release(Key.LeftAlt);
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        bool IsRuning = false;
+
+        private void Indicator_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsRuning)
+            {
+                myKeyEventHandeler = new System.Windows.Forms.KeyEventHandler(hook_KeyUp);
+                k_hook.KeyUpEvent += myKeyEventHandeler;//钩住键按下
+                k_hook.Hook_Start();//安装键盘钩子
+                IsRuning = true;
+                Indicator.Content = "关闭自动补全";
+            }
+            else
+            {
+                k_hook.Hook_Clear();//卸载键盘钩子
+                IsRuning = false;
+                Indicator.Content = "激活自动补全";
+            }
+        }
+    }
+}
